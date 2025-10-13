c830119b8a51f9ed990f248025c4c25155862e26
khalildemnati <48885642+khalildemnati@users.noreply.github.com>
2025-10-13 03:22:15 +0100

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true, lowercase: true },
+  passwordHash: { type: String, required: true },
+  balance: { type: Number, default: 0 }, // USD
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model("User", UserSchema);
