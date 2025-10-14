174d79659eefa8256524235005c53af0e1e61fe4
khalildemnati <48885642+khalildemnati@users.noreply.github.com>
2025-10-14 11:20:22 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,16 +1,10 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, unique: true, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  balance: { type: Number, default: 0 },
-}, { timestamps: true });
-
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
-export default mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+
+export default User;
